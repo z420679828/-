3574d17f8865e0a232beaa2ab545a8fc77fa833e
zhangbobo <420679828@qq.com>
2016-05-15 11:32:00 +0800

    Update AppDelegate.h
    随便看看

--- App/AppDelegate.h
+++ App/AppDelegate.h
@@ -14,6 +14,6 @@
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) SendMsgToWeChatViewController *viewController;
-@property (nonatomic,strong) NSString *str;
+
 
 @end
